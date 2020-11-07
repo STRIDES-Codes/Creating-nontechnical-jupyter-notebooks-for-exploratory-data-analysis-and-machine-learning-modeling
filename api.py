1e2ceddf111320e13710cb334f2cc6390a97bec9
Yogesh Dhande <yogeshkd@gmail.com>
2020-11-07 18:18:23 +0000

    Refactored api.py and added test_api.ipynb to reproduce main-tpm-interactive analysis using simple function calls

--- api.py
+++ api.py
@@ -21,6 +21,13 @@ LABEL_COLUMN_NAME = 'label 1'
 # TODO Define user inputs
 ## 
 
+
+def clean_up_index_labels(df):
+    # Replaces hyphens with underscores in labels
+    df.index = df.index.str.replace('-', '_')
+    return df
+
+
 def get_data():
     # Loads the data downloaded from the GDC Data Portal
     df_samples, df_counts = tc.load_gdc_data(sample_sheet_file, metadata_file, links_dir)
@@ -28,6 +35,9 @@ def get_data():
     # Add a labels column based on the project id and sample type 
     # columns and show the unique values by decreasing frequency
     df_samples[LABEL_COLUMN_NAME] = df_samples['project id'] + ', ' + df_samples['sample type']
+
+    clean_up_index_labels(df_samples)
+    clean_up_index_labels(df_counts)
 
     df_fpkm, df_fpkm_uq = tc.get_fpkm(df_counts, annotation_file, df_samples, links_dir)
     return df_samples, df_counts, df_fpkm, df_fpkm_uq
@@ -55,38 +65,34 @@ def perform_eda(samples):
     tc.eda_labels(samples)
 
 
-def clean_up_index_labels(df):
-    # Replaces hyphens with underscores in labels
-    df.index = df.index.str.replace('-', '_')
-    return df
-
-
 def calculate_tpm(counts):
     # Calculate the TPM using the counts and gene lengths
-    df_tpm = tc.get_tpm(df_counts, annotation_file)
+    df_tpm = tc.get_tpm(counts, annotation_file)
     df_tpm.shape
     return clean_up_index_labels(df_tpm)
 
 
-def perform_pca(counts_dataframe, labels_series):
+def perform_pca(counts_dataframe, samples):
+    labels_series = samples[LABEL_COLUMN_NAME]
     # Creates and plots PCA analyses using 
     # the variance-stabilizing-transformed data from DESeq2
     assert counts_dataframe.index.equals(labels_series.index)
     import sklearn.decomposition as sk_decomp
     pca = sk_decomp.PCA(n_components=10)
-    pca_res = pca.fit_transform(counts_dataframe)
+    pca_res = pca.fit_transform(counts_dataframe.iloc[:,:500])
     print('Top {} PCA explained variance ratios: {}'.format(10, pca.explained_variance_ratio_))
     ax = tc.plot_unsupervised_analysis(pca_res, labels_series)
     ax.set_title('PCA - variance-stabilizing transformation')
 
 
-def perform_tsne(counts_dataframe, labels_series):
+def perform_tsne(counts_dataframe, samples):
+    labels_series = samples[LABEL_COLUMN_NAME]
     # Creates and plots TSNE analyses using 
     # the variance-stabilizing-transformed data from DESeq2
     assert counts_dataframe.index.equals(labels_series.index)
     import sklearn.manifold as sk_manif
     tsne = sk_manif.TSNE(n_components=2)
-    tsne_res = tsne.fit_transform(counts_dataframe)  # TODO allow filtering?
+    tsne_res = tsne.fit_transform(counts_dataframe.iloc[:,:500])  # TODO allow filtering?
     ax = tc.plot_unsupervised_analysis(tsne_res, labels_series)
     ax.set_title('tSNE - variance-stabilizing transformation')
 
@@ -95,13 +101,15 @@ def perform_tsne(counts_dataframe, labels_series):
 if __name__ == '__main__':
     df_samples, df_counts, df_fpkm, df_fpkm_uq = get_data()
 
+    
+    # TODO allow selection of project id, tissues, types of cancer, categories
+    
     # TODO display summary statistics
-
 
     # Plot histograms of the numerical columns of the samples/labels before 
     # and after cutoffs are applied, and print out a summary of what was removed
     perform_eda(df_samples)
-    apply_cutoffs(df_samples, df_counts, df_fpkm, df_fpkm_uq)
+    df_samples, df_counts, df_fpkm, df_fpkm_uq = apply_cutoffs(df_samples, df_counts, df_fpkm, df_fpkm_uq)
     perform_eda(df_samples)
 
 
@@ -113,13 +121,9 @@ if __name__ == '__main__':
 
     df_tpm = calculate_tpm(df_counts)
 
-
-    clean_up_index_labels(df_samples)
-    clean_up_index_labels(df_counts)
-
     # TODO Run the variance-stabilizing transformation using DESeq2
     # using this most-detailed set of labels
 
-    
-    perform_pca(df_tpm, df_samples[LABEL_COLUMN_NAME])
-    perform_tsne(df_tpm, df_samples[LABEL_COLUMN_NAME])
+
+    perform_pca(df_tpm, df_samples)
+    perform_tsne(df_tpm, df_samples)
